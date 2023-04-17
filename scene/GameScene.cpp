f465dee13f660f1eedaf27e97d1401e54b91704b
k022g0060@g.neec.ac.jp <k022g0060@g.neec.ac.jp>
2023-04-17 16:26:44 +0900

    01_02_ライン描画

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,11 +1,12 @@
 #include "GameScene.h"
 #include "ImGuiManager.h"
+#include "PrimitiveDrawer.h"
 #include "TextureManager.h"
 #include <cassert>
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() { 
+GameScene::~GameScene() {
 	delete sprite_;
 	delete model_;
 }
@@ -14,50 +15,52 @@ void GameScene::Initialize() {
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
-	
-	//ファイル名を指定してテクスチャを読み込む
+
+	// ファイル名を指定してテクスチャを読み込む
 	textureHandle_ = TextureManager::Load("sample.png");
 
-	//スプライトの生成
+	// スプライトの生成
 	sprite_ = Sprite::Create(textureHandle_, {100, 50});
 
-	//3Dモデルの生成
+	// 3Dモデルの生成
 	model_ = Model::Create();
 
-	//ワールドトランスフォームの初期化
+	// ワールドトランスフォームの初期化
 	worldTransform_.Initialize();
-	//ビュープロジェクションの初期化
+	// ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 
-	//サウンドデータの読み込み
+	// サウンドデータの読み込み
 	soundDataHandle_ = audio_->LoadWave("mokugyo.wav");
 
 	////音声再生
-	//audio_->PlayWave(soundDataHandle_);
+	// audio_->PlayWave(soundDataHandle_);
 
-	//音声再生
+	// 音声再生
 	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
 
+	// ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
 }
 
 void GameScene::Update() {
 	ImGui::Begin("Debug1");
-	//ImGui::Text("Kamata Trou %d.%d.%d", 2050, 12, 31);
-	//float3入力ボックス
+	// ImGui::Text("Kamata Trou %d.%d.%d", 2050, 12, 31);
+	// float3入力ボックス
 	ImGui::InputFloat3("InputFloat3", inputFloat3);
-	//float3スライダー
+	// float3スライダー
 	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
 
 	ImGui::ShowDemoWindow();
 
 	ImGui::End();
 
-	//スプライトの今の座標を取得
+	// スプライトの今の座標を取得
 	Vector2 position = sprite_->GetPosition();
-	//座標を{ 2, 1 }移動
+	// 座標を{ 2, 1 }移動
 	position.x += 2.0f;
 	position.y += 1.0f;
-	//移動した座標をスプライトに反映
+	// 移動した座標をスプライトに反映
 	sprite_->SetPosition(position);
 
 	if (input_->TriggerKey(DIK_SPACE)) {
@@ -95,10 +98,24 @@ void GameScene::Draw() {
 
 	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
-
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
+
+	for (float x = 0; x < 100; x++) {
+		PrimitiveDrawer::GetInstance()->DrawLine3d(
+		    {0 - 50, 0 + x * 1 - 50, 0}, {100 - 50, 0 + x * 1 - 50, 0}, {1.0f, 0.0f, 0.0f, 1.0f}
+		);
+
+	}
+	for (float y = 0; y < 100; y++) {
+		PrimitiveDrawer::GetInstance()->DrawLine3d(
+		    {0 + y * 1 - 50, 0 - 50, 0}, {0 + y * 1 - 50, 100 - 50, 0}, {0.0f, 1.0f, 0.0f, 1.0f});
+	}
+	for (float z = 0; z < 100; z++) {
+		PrimitiveDrawer::GetInstance()->DrawLine3d(
+		    {0, 0, 0 + z * 100}, {0, 0, 100 + z * 100}, {0.0f, 0.0f, 1.0f, 1.0f});
+	}
 
 #pragma region 前景スプライト描画
 	// 前景スプライト描画前処理
@@ -107,7 +124,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	
+
 	sprite_->Draw();
 
 	// スプライト描画後処理
