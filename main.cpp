d791e8526dcc73c895608a46b64bcb5222105c4a
k022g0060@g.neec.ac.jp <k022g0060@g.neec.ac.jp>
2023-04-17 14:45:48 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2A_22_ワタナベ_レイヤ_AL3 ");
+	win->CreateGameWindow(L"LE2A_22_ワタナベ_レイヤ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
