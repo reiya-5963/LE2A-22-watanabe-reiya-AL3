13687803cbe61c79b0fb1a3f083965c2e48d7678
k022g0060@g.neec.ac.jp <k022g0060@g.neec.ac.jp>
2023-04-17 15:02:25 +0900

    01_02_スプライトの表示、移動

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -4,16 +4,29 @@
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { delete sprite_; }
 
 void GameScene::Initialize() {
-
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+	
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("sample.png");
+
+	//スプライトの生成
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	//スプライトの今の座標を取得
+	Vector2 position = sprite_->GetPosition();
+	//座標を{ 2, 1 }移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+	//移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+}
 
 void GameScene::Draw() {
 
@@ -53,6 +66,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
+	
+	sprite_->Draw();
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
