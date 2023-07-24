b7e8cd092ed7327cda9a61cc45a34218826575f7
k022g0060@g.neec.ac.jp <k022g0060@g.neec.ac.jp>
2023-07-24 14:44:57 +0900

    課題

--- DirectXGame/MyMath.cpp
+++ DirectXGame/MyMath.cpp
@@ -115,22 +115,11 @@ Vector3 MyMath::Perpendicular(const Vector3& vector) {
 }
 
 Vector3 MyMath::TransformNormal(Vector3 vector, Matrix4x4 matrix) {
-	Matrix4x4 result{};
-	result.m[0][0] = matrix.m[0][0] * vector.x;
-	result.m[0][1] = matrix.m[0][1] * vector.x;
-	result.m[0][2] = matrix.m[0][2] * vector.x;
-
-	result.m[1][0] = matrix.m[1][0] * vector.y;
-	result.m[1][1] = matrix.m[1][1] * vector.y;
-	result.m[1][2] = matrix.m[1][2] * vector.y;
-
-	result.m[2][0] = matrix.m[2][0] * vector.z;
-	result.m[2][1] = matrix.m[2][1] * vector.z;
-	result.m[2][2] = matrix.m[2][2] * vector.z;
+	Vector3 result{};
+	
 
 
-
-
+	return result;
 }
 
 
